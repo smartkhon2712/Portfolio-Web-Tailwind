e6a595db661d7a711074c741cc4114d99d3f4cc4
smartkhon2712 <smartkhon2712@gmail.com>
2024-05-05 14:30:43 +0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      primary: "#079211",
+      white: "#FFFFFF",
+      "light-gray": "#D1D5DB",
+      gray: "#808080",
+      "deep-gray": "#0A0A0F",
+      "light-dark": "#313131",
+      dark: "#101017",
+      "deep-dark": "#050507",
+    },
+  },
+};
